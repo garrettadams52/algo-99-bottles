5e1ea0b1152e24927f907e8ab66dc501b84be922
Garrett Adams <garrett.adams52@gmail.com>
2022-05-25 16:47:00 -0400

    python finished without recursion

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,6 +1,24 @@
 def bottle_song(n):
 	# write your code here!
-	while n >= 0
-		print(f'')
+	num = n
+	
+	while n > 2:
+		str = f"""
+		{n} bottles of beer on the wall, {n} bottles of beer.
+		Take one down and pass it around, {n-1} bottles of beer on the wall.
+		{n-1} bottles of beer on the wall, {n-1} bottles of beer."""
 
-	bottle_song()
+		print(str)
+		n = n - 1
+
+	str = f"""
+	\t2 bottles of beer on the wall, 2 bottles of beer.
+	\tTake one down and pass it around, 1 bottle of beer on the wall.
+	\t1 bottle of beer on the wall, 1 bottle of beer. 
+	\n\t\tTake one down and pass it around, no more bottles of beer on the wall.
+	\tNo more bottles of beer on the wall, no more bottles of beer.
+	\tGo to the store and buy some more, {num} bottles of beer on the wall"""
+
+	print(str)
+	
+print(bottle_song(9))
